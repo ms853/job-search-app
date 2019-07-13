fbe4a1b85a4dccb1b17bfd51993f2f3333495845
ms853 <ms853@student.le.ac.uk>
2019-07-13 01:28:37 +0100

    updated Slides and Welcome components and completed the functionality

--- job-search/components/Slides.js
+++ job-search/components/Slides.js
@@ -1,24 +1,46 @@
 import React, {Component} from 'react';
 import {Text, View, ScrollView, Dimensions} from 'react-native';
+import { Button } from "react-native-elements";
 
 const WINDOW_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
    
+    //Method to render a button for the last slide
+    renderButtonForLastSlide(index) {
+    
+        //-1 for the last index.
+        if(index === this.props.data.length -1) {
+            return (
+            <Button 
+                title="I am ready" 
+                raised 
+                buttonStyle={style.buttonStyle}
+                onPress={() => this.props.onComplete}
+            />
+            );
+        }
+    }
 
    _renderSlidesData() {
-        return this.props.data.map((slide) => {
-            <View key={slide.text}> style={style.slideStyle}
+        return this.props.data.map((slide, index) => {
+            <View 
+            key={slide.text}
+            style={[style.slideStyle, {backgroundColor: slide.color}]}> 
                 <Text style={style.textStyle}>{slide.text}</Text>
+                {this.renderButtonForLastSlide(index)}
             </View>
         });
    }
+
+  
 
     render() {
     return (
         <ScrollView
             horizontal
             style={{ flex: 1 }}
+            pagingEnabled
         >
             {this._renderSlidesData()}
         </ScrollView>    
@@ -29,13 +51,18 @@ class Slides extends Component {
 const style = {
     textStyle: {
         fontSize: 30,
-        color: 'black'
+        color: 'white',
+        alignItems: 'center'
     }, 
     slideStyle: {
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
         width: WINDOW_WIDTH //so it covers the whole width of the screen. 
+    },
+    buttonStyle: {
+        marginTop: 15,
+        backgroundColor: '#288D1'
     }
 }
 
--- job-search/screens/WelcomeScreen.js
+++ job-search/screens/WelcomeScreen.js
@@ -2,20 +2,26 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Slides from '../components/slides';
 
+
+
 //Data that will be displayed on the welcome page.
 const SLIDE_DATA = [
-{text: 'Welcome to Job Search!'},
-{text: 'Number 1 Job Searching App'},
-{text: 'Set your location, then swipe away.'}
+{text: 'Welcome to Job Search!', color: '#03A9F4'},
+{text: 'Number 1 Job Searching App', color: '#009688'},
+{text: 'Set your location, then swipe away.', color: '#03A9F4'}
 ];
 
 class WelcomeScreen extends Component {
  
+    onSlidesComplete = () => {
+        this.props.navigation.navigate('Auth');
+    }
+
     render() {
         return (
             //data fed to component. 
             <View>
-                <Slides data={SLIDE_DATA} />
+                <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />
             </View>
         );
     }
